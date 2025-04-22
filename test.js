461ae16d39f2a183e5d1c0901ba1d894271dadd9
Y.Guta <yevguta@gmail.com>
2025-04-22 12:20:34 +0200

    add test.js  k6 testing DOP

--- test.js
+++ test.js
@@ -4,6 +4,13 @@ import { check, sleep } from 'k6';
 export let options = {
 	  vus: 10, // virtual users
 	  duration: '30s',
+	  // iteration: 6,    // можно ставить количество итераций. оно не может быть меньше числа vus                               (виртуальных юзеров)
+	//    stages: [
+	//    { duration: '10s', target: '10'}, // в течении 10с одновременно 10 пользователей
+	//    { duration: '10s', target: '75'}, // в течении 10с одновременно 75 пользователей
+	//    { duration: '5s', target: '0'},  //  в течении 5с - никого
+	
+	//    ]
 };
 
 export default function () {
